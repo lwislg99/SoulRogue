449c409b36911120e89235145506a803b8e09176
Manuel Santiso <manuelsantiso0@gmail.com>
2021-04-11 23:27:39 +0200

    inventory

---
+++ Assets/Manuel/Inventario/Inventario.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+    public List<GameObject> Bag = new List<GameObject>();
+    GameObject Inv;
+    public bool Activar_inv;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Item"))
+        {
+            for (int i = 0; i = Bag.Count; i++)
+            {
+                if (Bag[i].GetComponent<Image>().enabled == false)
+                {
+                    Bag[i].GetComponent<Image>().enabled == true;
+                    Bag[i].GetComponent<Image>().sprite = coll.GetComponent<SpriteRenderer>().sprite;
+                    break;
+                }
+
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Activar_inv)
+        {
+            Inv.SetActive(true)
+        }
+        else
+        {
+            Inv.SetActive(false)
+
+        }
+        if (Imput.GetKeyDown(KeyCode.Return))
+        {
+            Activar_inv =!Activar_inv
+        }
+    }
+        
+    
+}
