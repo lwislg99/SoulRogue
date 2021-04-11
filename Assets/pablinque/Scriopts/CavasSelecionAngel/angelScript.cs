60074bcae98f090b23a690f4156b8d40777288da
Pablinque <33763207+Pablinque@users.noreply.github.com>
2021-04-11 20:49:55 +0200

    Pablinque Entrega 11/4/2021

---
+++ Assets/pablinque/Scriopts/CavasSelecionAngel/angelScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angelScript : MonoBehaviour
+{
+    public string esteAngel;
+    
+
+    public Player PlayerI;
+    public SelecionAngeles SelecionAngelesI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void pulsarBoton()
+    {
+
+        PlayerI = FindObjectOfType<Player>();
+        PlayerI.movJugadorActivado = true;
+        SelecionAngelesI = FindObjectOfType<SelecionAngeles>();
+
+
+        PlayerI.angelGame = esteAngel;
+
+        SelecionAngelesI.button1.gameObject.SetActive(false);
+        SelecionAngelesI.button2.gameObject.SetActive(false);
+        SelecionAngelesI.button3.gameObject.SetActive(false);
+
+    }
+}
