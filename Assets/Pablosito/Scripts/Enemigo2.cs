bd48b5254b47071cb704ab652123a60d66a9e4bd
Manuel Santiso <manuelsantiso0@gmail.com>
2021-04-26 14:54:31 +0200

    aoa

--- Assets/Pablosito/Scripts/Enemigo2.cs
+++ Assets/Pablosito/Scripts/Enemigo2.cs
@@ -120,13 +120,13 @@ public class Enemigo2 : MonoBehaviour
     }
 
     private void LateUpdate()
-    {
+    {//Esto es para las animaciones del movimiento
         float velocityX = agent.velocity.x;
         float velocityY = agent.velocity.y;
 
         anim.SetFloat("MovX", velocityX);
         anim.SetFloat("MovY", velocityY);
-
+        //ESto es para las animaciones de ataque,explosion
         Vector2 Direccionplayer = playeri.transform.position - this.transform.position;
         Direccionplayer.Normalize();
         float posicionx = Direccionplayer.x;
