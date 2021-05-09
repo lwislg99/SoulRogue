a37541da1226d9769829a6332888f57b95035a3c
pablosito <pablogonzaloarruti@gmail.com>
2021-05-09 21:17:14 +0200

    mapas y cosas

--- Assets/Pablosito/Scripts/RoomSpawner.cs
+++ Assets/Pablosito/Scripts/RoomSpawner.cs
@@ -65,21 +65,25 @@ public class RoomSpawner : MonoBehaviour
             
             if (openingDirection == 1)
             {
+                Debug.Log("tu puta madre 1");
                 rand = Random.Range(0, templates.bottomRooms.Length);
                 Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
             }
             else if (openingDirection == 2)
             {
+                Debug.Log("tu puta madre 2");
                 rand = Random.Range(0, templates.topRooms.Length);
                 Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
             }
             else if (openingDirection == 3)
             {
+                Debug.Log("tu puta madre 3");
                 rand = Random.Range(0, templates.leftRooms.Length);
                 Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
             }
             else if (openingDirection == 4)
             {
+                Debug.Log("tu puta madre 4");
                 rand = Random.Range(0, templates.rightRooms.Length);
                 Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
                 
