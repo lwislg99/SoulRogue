9337f7351e2eede45bb00ed9366944cefc5f7512
Pablinque <33763207+Pablinque@users.noreply.github.com>
2021-05-16 17:41:02 +0200

    ptq final

--- Assets/pablinque/Scripts/Personaje/zonaWin.cs
+++ Assets/pablinque/Scripts/Personaje/zonaWin.cs
@@ -8,6 +8,9 @@ public class zonaWin : MonoBehaviour
     public Player playerI;
     public GameObject agujero;
     public bool yaTienes30=false;
+    public bool tuto = false;
+    public bool level1 = false;
+    public bool level2 = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class zonaWin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerI.numeroMuertes >= 30&&yaTienes30==false)
+        if (playerI.numeroMuertes >= 30&&yaTienes30==false||tuto==true && yaTienes30 == false)
         {
             Instantiate(agujero, this.transform);
             yaTienes30 = true;
@@ -27,10 +30,22 @@ public class zonaWin : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D collision)
     {
         
-        if (playerI.numeroMuertes >= 30)
+        if (playerI.numeroMuertes >= 30||tuto==true)
         {
+            if(tuto==true)
+            {
+                SceneManager.LoadScene("Nivel1");
+            }
+            else if(level1==true)
+            {
+                SceneManager.LoadScene("Nivel2");
+            }
+            else if(level2==true)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
             
-            SceneManager.LoadScene("MainMenu");
+            
         }
     }
 }
